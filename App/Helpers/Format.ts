d69cff8d18dc50d2fb0f85f24401484ff3106c30
An Mai <an.mh@viralworks.com>
2019-03-25 16:50:32 +0700

    Remove array for error format

--- App/Helpers/Format.ts
+++ App/Helpers/Format.ts
@@ -1,17 +1,8 @@
-export function formatError(err:any, status:number = 500) {
-    if (typeof err  == 'string') { 
-        return {
-            status: status,
-            errors: {
-                message: err
-            }
-        };
-    } else {
-        return {
+export function formatError(err:string, status:number = 500) {
+    return {
             status: status,
             errors: err
         };
-    };
 }
 
 export function formatResult(result:any, status:number = 200) {
