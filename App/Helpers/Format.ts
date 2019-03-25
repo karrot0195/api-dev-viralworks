037bc5d658c393b1e287258b2f4f1570e049658a
An Mai <an.mh@viralworks.com>
2019-03-25 13:37:54 +0700

    Add helper format response

---
+++ App/Helpers/Format.ts
@@ -0,0 +1,31 @@
+export function formatError(err:any, status:number = 500) {
+    if (typeof err  == 'string') { 
+        return {
+            status: status,
+            errors: {
+                message: err
+            }
+        };
+    } else {
+        return {
+            status: status,
+            errors: err
+        };
+    };
+}
+
+export function formatResult(result:any, status:number = 500) {
+    if (typeof result  == 'string') { 
+        return {
+            status: status,
+            results: {
+                message: result
+            }
+        };
+    } else {
+        return {
+            status: status,
+            results: result
+        };
+    };
+}
