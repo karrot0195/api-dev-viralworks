42450ed5eb81cbd3bfa4e41eeef949ec58240ee1
Ain <an.mh@viralworks.com>
2019-03-28 21:07:49 +0700

    Rework query parser

--- Helpers/Format.ts
+++ Helpers/Format.ts
@@ -1,31 +1,56 @@
-import { Request } from 'express';
+/*
+Parsing URL queries to data that can be used in Mongoose
 
-export function parseQueryData(req: Request): any {
-    let condition: any = {};
-    condition.options = {};
-    condition.search = {};
+Example: Model.find(conditions, {}, options)
 
-    if (req.query.sort && req.query.order) {
-        let order = 1;
-        if (req.query.order == 'desc') order = -1;
-
-        let sort = {};
-        sort[req.query.sort] = order;
-
-        condition.options.sort = sort;
+Code above will find value match conditions then paginate and sort using options
+*/
+export function processQuery(query: any, fields: Array<string>): any {
+    const result: any = {};
+    result.options = {};
+    result.conditions = {};
+    if (typeof query == 'object') {
+        result.options.page = query.page || 0;
+        result.options.limit = query.limit || 0;
+        result.options.skip = result.options.page * result.options.limit;
+        result.options.sort = processSort(query.sort);
+        result.conditions = processValue(query.value, query.term, fields);
     }
 
-    if (req.query.limit !== null && req.query.limit !== '') {
-        if (req.query.page !== null && req.query.page !== '') condition.options.skip = req.query.page * req.query.limit;
+    return result;
+}
 
-        condition.options.limit = req.query.limit;
+function processValue(value: any, term: any, fields: Array<string>) {
+    let result: any = {};
 
-        condition.options.page = req.query.page | 0;
+    if (term && fields) {
+        result.$or = [];
+        fields.forEach(item => {
+            let tmp: any = {};
+            tmp[item] = { $regex: term, $options: 'i' };
+            result.$or.push(tmp);
+        });
     }
 
-    if (req.query.term) {
-        condition.search.$text = { $search: req.query.term };
+    if (value) {
+        value.split(',').map(item => {
+            let tmp = item.split('|');
+            if (result[tmp[0]] === undefined) result[tmp[0]] = {};
+            if (result[tmp[0]].$in === undefined) result[tmp[0]].$in = [];
+            result[tmp[0]].$in.push(tmp[1]);
+        });
     }
+    return result;
+}
 
-    return condition;
+function processSort(sort: any) {
+    if (sort) {
+        let result = {};
+        sort.split(',').map(item => {
+            let tmp = item.split('|');
+            result[tmp[0]] = 1;
+            if (tmp[1] == 'desc') result[tmp[0]] = -1;
+        });
+        return result;
+    }
 }
