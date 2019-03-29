50af09d9d7a902144821499ca6996b5ccfeabea8
Ain <an.mh@viralworks.com>
2019-03-29 16:47:43 +0700

    Update rbac error

--- System/Enum/Error.ts
+++ System/Enum/Error.ts
@@ -1,5 +1,6 @@
 export enum RBACErrorMessage {
     PATH_NOT_FOUND = "Path isn't existed",
     PARENT_NOT_FOUND = "Parent role isn't existed",
-    USER_NOT_FOUND = "User isn't existed"
+    USER_NOT_FOUND = "User isn't existed",
+    ROLE_NOT_FOUND = "Role isn't existed"
 }
