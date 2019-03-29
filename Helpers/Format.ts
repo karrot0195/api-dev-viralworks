3c87706d0a94364e42e2dac5156f2e9ded20b984
Ain <an.mh@viralworks.com>
2019-03-29 09:28:42 +0700

    Add selection to query parse

--- Helpers/Format.ts
+++ Helpers/Format.ts
@@ -1,20 +1,26 @@
 /*
 Parsing URL queries to data that can be used in Mongoose
 
-Example: Model.find(conditions, {}, options)
+Example: Model.find(conditions, projections, options)
 
-Code above will find value match conditions then paginate and sort using options
+Code above will find value match conditions, paginate and sort using options, select fields on projections
 */
 export function processQuery(query: any, fields: Array<string>): any {
     const result: any = {};
+
     result.options = {};
     result.conditions = {};
+    result.projections = {};
+
     if (typeof query == 'object') {
         result.options.page = query.page || 0;
         result.options.limit = query.limit || 0;
         result.options.skip = result.options.page * result.options.limit;
         result.options.sort = processSort(query.sort);
+
         result.conditions = processValue(query.value, query.term, fields);
+
+        result.projections = processField(query.field);
     }
 
     return result;
@@ -25,6 +31,7 @@ function processValue(value: any, term: any, fields: Array<string>) {
 
     if (term && fields) {
         result.$or = [];
+
         fields.forEach(item => {
             let tmp: any = {};
             tmp[item] = { $regex: term, $options: 'i' };
@@ -35,6 +42,7 @@ function processValue(value: any, term: any, fields: Array<string>) {
     if (value) {
         value.split(',').map(item => {
             let tmp = item.split('|');
+
             if (result[tmp[0]] === undefined) result[tmp[0]] = {};
             if (result[tmp[0]].$in === undefined) result[tmp[0]].$in = [];
             result[tmp[0]].$in.push(tmp[1]);
@@ -44,13 +52,25 @@ function processValue(value: any, term: any, fields: Array<string>) {
 }
 
 function processSort(sort: any) {
+    let result = {};
+
     if (sort) {
-        let result = {};
         sort.split(',').map(item => {
             let tmp = item.split('|');
             result[tmp[0]] = 1;
             if (tmp[1] == 'desc') result[tmp[0]] = -1;
         });
-        return result;
     }
+    return result;
+}
+
+function processField(fields: any) {
+    let result = {};
+
+    if (fields) {
+        fields.split(',').map(item => {
+            result[item] = 1;
+        });
+    }
+    return result;
 }
