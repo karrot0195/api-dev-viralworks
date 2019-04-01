7473951393b295789c47d3628928d6dbeec16919
Ain <an.mh@viralworks.com>
2019-04-01 17:37:59 +0700

    Update unauthorizate

--- System/Error/Unauthorized.ts
+++ System/Error/Unauthorized.ts
@@ -2,7 +2,7 @@ import { BaseError } from '../Error';
 import { CommonErrorMessage } from 'System/Enum/Error';
 
 export class Unauthorized extends BaseError {
-    constructor() {
-        super(CommonErrorMessage.E401, 401);
+    constructor(message: string = CommonErrorMessage.E401) {
+        super(message, 401);
     }
 }
