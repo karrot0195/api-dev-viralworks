98901de890e9399d2cafb2e50f51ab47307d830d
karrot <hieu.tran@viralworks.com>
2019-04-04 10:23:31 +0700

    add api delete category reason + reason

--- App/Modules/Admin/Services/CategoryReasonService.ts
+++ App/Modules/Admin/Services/CategoryReasonService.ts
@@ -3,7 +3,7 @@ import { Config } from 'System/Config';
 import { Mongo } from 'System/Mongo';
 import { CategoryReasonModel, ICategoryReason, IReason } from 'App/Models/CategoryReasonModel';
 import * as _ from 'lodash';
-import { Conflict } from 'System/Error';
+import { Conflict, NotFound } from 'System/Error';
 
 @Injectable
 export class CategoryReasonService {
@@ -12,7 +12,7 @@ export class CategoryReasonService {
         private readonly _mongo: Mongo,
         private readonly _categoryReasonModel: CategoryReasonModel
     ) {}
-    
+
     createCategoryReason(data: ICategoryReason) {
         return this._categoryReasonModel.create(data);
     }
@@ -37,7 +37,7 @@ export class CategoryReasonService {
             if (obj.name == data.name) {
                 throw new Conflict('Dulicate reason');
             }
-        })
+        });
         catReason.reasons.push(data);
         return catReason.save();
     }
@@ -45,7 +45,7 @@ export class CategoryReasonService {
     async updateReason(catReason: any, idx: number, data: IReason) {
         Object.keys(data).forEach(k => {
             _.set(catReason.reasons[idx], k, data[k]);
-        })
+        });
         const result = await catReason.save();
         return result.reasons[idx];
     }
@@ -57,8 +57,29 @@ export class CategoryReasonService {
                 idx = i;
                 return;
             }
-        })
+        });
         return idx;
     }
-    
+
+    async removeCategoryReason(categoryId: string) {
+        const catgoryReason = await this._categoryReasonModel.findById(categoryId);
+        if (!catgoryReason) {
+            throw new NotFound('Not found category reason by id');
+        }
+
+        return catgoryReason.remove();
+    }
+
+    async deleteReason(categoryId: string, reasonId: string) {
+        const catgoryReason = await this._categoryReasonModel.findById(categoryId);
+        if (!catgoryReason) {
+            throw new NotFound('Not found category reason by id');
+        }
+        catgoryReason.reasons.forEach((reason, idx) => {
+            if (reason._id == reasonId) {
+                catgoryReason.reasons.splice(idx, 1);
+            }
+        });
+        return await catgoryReason.save();
+    }
 }
