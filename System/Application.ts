c60f34abbedecdb39280bad719f1b3fe71193441
ThanhHieu0195 <thanhhieu0195@gmail.com>
2019-04-20 09:09:58 +0700

    update 1123

--- System/Application.ts
+++ System/Application.ts
@@ -95,7 +95,7 @@ export class Application {
 
         // Config CORS
         this._app.use((req: Request, res: Response, next: NextFunction) => {
-            res.header('Access-Control-Allow-Origin', '*');
+            res.header('Access-Control-Allow-Origin', 'https://api.dev.viralworks.com');
             res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
             res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
             next();
